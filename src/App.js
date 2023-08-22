683a434299c49fcbc5ede602fd6874d512b4cda3
DreamSky1996 <webhipe@gmail.com>
2023-08-23 01:17:21 +0900

    toogle theme app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useContext } from 'react';
 import './App.css';
+import { ThemeContext } from './contexts/theme';
+import ToggleBtn from './components/ToggleBtn/toggleBtn';
 
 function App() {
+  const [{ themeName }] = useContext(ThemeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${themeName} app`}>
+      <div className="card">
+        <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+          <ToggleBtn />
+        </div>
+        <h1>Toggle Theme App</h1>
+        <h4>Dark Mode is {themeName === 'dark' ? 'On' : 'Off'}</h4>
+      </div>
     </div>
   );
 }
